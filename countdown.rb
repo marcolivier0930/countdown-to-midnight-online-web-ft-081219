5d0dfdbd3471d5158ff28db8859256651c3abd77
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-15 19:55:03 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,7 +2,7 @@
 
 def countdown(number = 12)
   while number > 0
-    puts "#{number} SECONDS!"
+    puts "#{number} SECOND(S)!"
     number -= 1
   end
   return "HAPPY NEW YEAR!"
@@ -11,7 +11,7 @@ end
 
 def countdown_with_sleep(count = 5)
   while count > 0
-    puts "#{count} SECONDS!"
+    puts "#{count} SECOND(S)!"
     sleep 1
     count -= 1
   end
