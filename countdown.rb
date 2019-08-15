69102bfe57e43236485ec4a498c2bbd8ba7fe60e
Marc Blanchard <marcblanchard0930@gmail.com>
2019-08-15 19:37:39 +0000

    Done.

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown
-  count = 10 
-  
-  while count > 0 
-    puts "#{count}"
+  while count == 0 
+    puts "#{count} SECONDS(S)!"
     count -= 1 
   end
   return "HAPPY NEW YEARS!"
 end
+   
